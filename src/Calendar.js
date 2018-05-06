8a704e586731d3ba37ed76a628ea8247f99d9e71
SpartanPlume <robin.charpentier@epitech.eu>
2018-05-07 00:55:16 +0900

    Add calendar

---
+++ src/Calendar.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import BigCalendar from 'react-big-calendar';
+import Graph from './Graph';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+let allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k]);
+
+class Calendar extends Graph {
+    render() {
+        return (
+            <div ref={this.myRef} style={{width: this.state.width, height: this.state.height}}>
+                <BigCalendar
+                    events={this.state.data}
+                    view='week'
+                    showMultiDayTimes
+                    defaultDate={new Date()}
+                />
+            </div>
+        );
+    }
+}
+
+export default Calendar;
