69750c446bcebafca4c37ca8cd28ca8b32e59f42
SpartanPlume <robin.charpentier@epitech.eu>
2018-05-08 23:33:25 +0900

    Add tooltip, legend and labels to HalfPieGraph

--- src/HalfPieGraph.js
+++ src/HalfPieGraph.js
@@ -7,14 +7,18 @@ const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
 class HalfPieGraph extends Graph {
     render() {
-        var label = this.props.label ? (<LabelList dataKey="name" position="inside" fill="#ffffff"/>) : null;
+        var legend = this.props.legend ? (<Legend align="center"/>) : null;
+        var tooltip = this.props.tooltip ? (<Tooltip/>) : null;
+        var label = this.props.label ? (<LabelList dataKey="percent" position="inside" fill="#ffffff"/>) : null;
         return (
             <div ref={this.myRef} style={{width: this.state.width, height: this.state.height}}>
                 <PieChart width={this.state.pixelWidth} height={this.state.pixelHeight} margin={{top: 20, right: 20, bottom: 20, left: 20}}>
-                    <Pie startAngle={180} endAngle={0} data={this.state.data} nameKey="name" dataKey="value">
+                    <Pie startAngle={180} endAngle={0} innerRadius={40} outerRadius={80} data={this.state.data} nameKey="name" dataKey="percent" label>
                         {this.state.data.map((entry, index) => <Cell key={index} fill={this.props.colors[index % this.props.colors.length]}/>)}
                         {label}
                     </Pie>
+                    {tooltip}
+                    {legend}
                 </PieChart>
             </div>
         );
@@ -28,7 +32,9 @@ HalfPieGraph.propTypes = {
 HalfPieGraph.defaultProps = {
     width: "100%",
 	height: "100%",
-    label: true,
+    label: false,
+    legend: true,
+    tooltip: true,
     colors: COLORS
 };
 
