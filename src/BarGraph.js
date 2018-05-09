fa669e4e34bc2c4aea6be8b12f96ded6c17be737
SpartanPlume <robin.charpentier@epitech.eu>
2018-05-09 22:19:09 +0900

    BarGraph and ColumnGraph show all ticks and BarGraph resize if necessary

--- src/BarGraph.js
+++ src/BarGraph.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { ComposedChart, Bar, XAxis, YAxis, Tooltip } from 'recharts';
 import Graph from './Graph';
 
+var BAR_SIZE = 30;
+
 function getFont() {
 	const div = document.querySelector('body');
 	const style = window.getComputedStyle(div);
@@ -16,6 +18,32 @@ function getTextWidth(text) {
 	return Math.ceil(metric.width * 1.1); // measureText method's return value may be slightly underestimated, add some pixels or scale it
 }
 
+/* if scrollbar
+function getScrollBarWidth () {
+	var inner = document.createElement('p');
+	inner.style.width = "100%";
+	inner.style.height = "200px";
+  
+	var outer = document.createElement('div');
+	outer.style.position = "absolute";
+	outer.style.top = "0px";
+	outer.style.left = "0px";
+	outer.style.visibility = "hidden";
+	outer.style.width = "200px";
+	outer.style.height = "150px";
+	outer.style.overflow = "hidden";
+	outer.appendChild (inner);
+  
+	document.body.appendChild (outer);
+	var w1 = inner.offsetWidth;
+	outer.style.overflow = 'scroll';
+	var w2 = w1 === inner.offsetWidth ? outer.clientWidth : inner.offsetWidth;
+  
+	document.body.removeChild (outer);
+  
+	return (w1 - w2);
+};*/
+
 class BarGraph extends Graph {
 	render() {
 		var maxWidth = 0;
@@ -26,12 +54,13 @@ class BarGraph extends Graph {
 				maxWidth = tmpWidth;
 			}
 		}
+		var height = (BAR_SIZE + 1) * this.state.data.length;
 		return (
-			<div id="bar-graph" ref={this.myRef} style={{width: this.state.width, height: this.state.height}}>
-				<ComposedChart layout="vertical" width={this.state.pixelWidth} height={this.state.pixelHeight} data={this.state.data} margin={{top: 20, right: 20, bottom: 20, left: 20}}>
+			<div id="bar-graph" ref={this.myRef} style={{width: this.state.width, height: height}}>
+				<ComposedChart layout="vertical" width={this.state.pixelWidth} height={height} data={this.state.data} margin={{top: 20, right: 20, bottom: 20, left: 20}}>
 					<XAxis type="number"/>
-					<YAxis dataKey="process" type="category" width={maxWidth}/>
-					<Bar dataKey="length" fill="#8884d8" />
+					<YAxis dataKey="process" type="category" width={maxWidth} interval={0}/>
+					<Bar dataKey="length" fill="#8884d8"/>
 					<Tooltip cursor={false}/>
 				</ComposedChart>
 			</div>
