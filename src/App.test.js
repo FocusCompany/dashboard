2288582d635b03ac9be042ffc643d94d237fd215
SpartanPlume <spartan.plume@gmail.com>
2018-11-05 20:08:32 +0100

    Add storybook + storyshots

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import "raf/polyfill";
+import initStoryshots from '@storybook/addon-storyshots';
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+initStoryshots();
