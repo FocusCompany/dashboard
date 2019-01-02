89d2b891df69520702a5c51097bf25ff048c7713
Etienne Pasteur <etienne.pasteur@epitech.eu>
2019-01-02 22:54:00 +0100

    wip

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,16 @@
-import {Component, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import * as Chartist from 'chartist';
 import 'chartist-plugin-legend';
 import {Dnd, Process} from '../_models/stats';
 import {StatsService} from '../_services/stats.service';
-import {NgSelectModule} from '@ng-select/ng-select';
 import {DevicesService} from '../_services/devices.service';
 import {Device} from '../_models/device';
 import {CollectionsService} from '../_services/collections.service';
 import {Collection} from '../_models/collection';
 import * as moment from 'moment';
+import 'moment-duration-format';
 import {SatDatepickerInputEvent, SatDatepickerRangeValue} from 'saturn-datepicker';
+import * as _ from 'lodash';
 
 @Component({
     selector: 'app-dashboard',
@@ -28,26 +29,11 @@ export class DashboardComponent implements OnInit {
     hoursWorked = '0';
     hoursAfk = '0';
     productivityScore = 0;
-    timeLastUpdate = 0;
-
-    dataHeatMap = [{
-        'date': '2016-01-01',
-        'total': 17164,
-        'details': [{
-            'name': 'Project 1',
-            'date': '2016-01-01 12:30:45',
-            'value': 9192
-        }, {
-            'name': 'Project 2',
-            'date': '2016-01-01 13:37:00',
-            'value': 6753
-        },
-            {
-                'name': 'Project N',
-                'date': '2016-01-01 17:52:41',
-                'value': 1219
-            }]
-    }];
+    overview = 'day';
+    dataHeatMap = [];
+    isDataActivitySummaryChart = false;
+    isDataTimeSpentChart = false;
+    isDataProcessChart = false;
 
     constructor(private statsService: StatsService, private devicesService: DevicesService,
                 private collectionsService: CollectionsService) {
@@ -73,7 +59,7 @@ export class DashboardComponent implements OnInit {
         this.refreshData();
     }
 
-    updateCharts(dataActivitySummaryChart: any, dataTimeSpentChart: any, dataBarChart: any) {
+    updateCharts(dataActivitySummaryChart: any, dataTimeSpentChart: any, dataProcessChart: any) {
         const activitySummaryChart = new Chartist.Pie('#activitySummaryChart', dataActivitySummaryChart, {
             donut: true,
             donutWidth: 40,
@@ -95,7 +81,7 @@ export class DashboardComponent implements OnInit {
             ]
         });
 
-        const barChart = new Chartist.Bar('#barChart', dataBarChart, {
+        const processChart = new Chartist.Bar('#processChart', dataProcessChart, {
             horizontalBars: true,
             distributeSeries: true,
             axisX: {
@@ -110,33 +96,84 @@ export class DashboardComponent implements OnInit {
                 offset: 100
             },
             width: '100%',
-            height: dataBarChart.series.length * 45 + 'px',
+            height: dataProcessChart.series.length * 45 + 'px',
         }).on('draw', function (data) {
             if (data.type === 'bar') {
+                data.element._node.onclick = () => {
+                    console.log(data.axisY.ticks[data.seriesIndex]);
+                };
                 data.element.attr({
                     style: 'stroke-width: 30px'
                 });
             }
         });
+    }
 
+    updateHeatMap(process: Array<Process>) {
+        if (process) {
+            const dataHeatMap = [];
+            process.forEach((obj: Process) => {
+                if (obj.afk === false) {
+                    const activity: any = {};
+                    activity.date = moment(obj.start).format('YYYY-MM-DD');
+                    activity.details = {
+                        'name': obj.process,
+                        'date': moment(obj.start).format('YYYY-MM-DD HH:mm:ss'),
+                        'value': moment.duration(moment(obj.end).diff(moment(obj.start))).asSeconds()
+                    };
+                    dataHeatMap.push(activity);
+                }
+            });
+            console.log(dataHeatMap);
+
+            dataHeatMap.forEach((data: any) => {
+                let addOrNot = true;
+                this.dataHeatMap.forEach((obj: any) => {
+                    if (obj.date === data.date) {
+                        addOrNot = false;
+                        obj.total += data.details.value;
+                        obj.details.push(data.details);
+                    }
+                });
+                if (addOrNot === true) {
+                    this.dataHeatMap.push({'date': data.date, 'total': data.details.value, 'details': [data.details]});
+                    addOrNot = false;
+                }
+            });
+        }
+        console.log(this.dataHeatMap);
     }
 
     refreshData() {
+        this.dataHeatMap = [];
+        this.isDataActivitySummaryChart = false;
+        this.isDataTimeSpentChart = false;
+        this.isDataProcessChart = false;
+
         const device = this.selectedDevice === 'ALL' ? null : this.selectedDevice;
         this.statsService.getProcess(device, this.selectedCollections, this.date.begin.toISOString(),
             moment(this.date.end).add(1, 'd').toISOString()).subscribe((process: Array<Process>) => {
             let durationWork = 0, durationAfk = 0;
             const dataTimeSpentChart = {labels: [], series: []};
-            const dataBarChart = {labels: [], series: []};
+            const dataProcessChart = {labels: [], series: []};
             const dataActivitySummaryChart = {labels: [], series: []};
             if (process) {
                 process.forEach((obj: Process) => {
                     if (obj.afk === false) {
                         durationWork += moment.duration(moment(obj.end).diff(moment(obj.start))).asSeconds();
-                        dataBarChart.labels.push(obj.process);
-                        dataBarChart.series.push(moment.duration(moment(obj.end).diff(moment(obj.start))).asMinutes());
-                        dataTimeSpentChart.labels.push(obj.process);
-                        dataTimeSpentChart.series.push(moment.duration(moment(obj.end).diff(moment(obj.start))).asMinutes());
+                        const indexProcess = _.indexOf(dataProcessChart.labels, obj.process);
+                        if (indexProcess === -1) {
+                            dataProcessChart.labels.push(obj.process);
+                            dataTimeSpentChart.labels.push(obj.process);
+                            dataProcessChart.series.push(moment.duration(moment(obj.end).diff(moment(obj.start))).asMinutes());
+                            dataTimeSpentChart.series.push(moment.duration(moment(obj.end).diff(moment(obj.start))).asMinutes());
+
+                        } else {
+                            dataProcessChart.series[indexProcess] += moment.duration(moment(obj.end).diff(moment(obj.start))).asMinutes();
+                            dataTimeSpentChart.series[indexProcess] += moment.duration(moment(obj.end).diff(moment(obj.start))).asMinutes();
+                        }
+                        this.isDataTimeSpentChart = true;
+                        this.isDataProcessChart = true;
                     } else if (obj.afk === true) {
                         durationAfk += moment.duration(moment(obj.end).diff(moment(obj.start))).asSeconds()
                     }
@@ -145,17 +182,22 @@ export class DashboardComponent implements OnInit {
             if (durationWork !== 0) {
                 dataActivitySummaryChart.labels.push('Work');
                 dataActivitySummaryChart.series.push(((durationWork * 100) / (durationWork + durationAfk)));
+                this.isDataActivitySummaryChart = true;
             }
             if (durationAfk !== 0) {
                 dataActivitySummaryChart.labels.push('Afk');
                 dataActivitySummaryChart.series.push(((durationAfk * 100) / (durationWork + durationAfk)));
+                this.isDataActivitySummaryChart = true;
             }
-            this.hoursWorked = moment.utc(durationWork * 1000).format('HH:mm');
-            this.hoursAfk = moment.utc(durationAfk * 1000).format('HH:mm');
+            // @ts-ignore
+            this.hoursWorked = moment.duration(durationWork, 'seconds').format('HH:mm', {trim: false});
+            // @ts-ignore
+            this.hoursAfk = moment.duration(durationAfk, 'seconds').format('HH:mm', {trim: false});
             const numberOfDays = moment.duration(moment(this.date.end).add(1, 'd').diff(this.date.begin)).asDays();
             this.productivityScore = (((moment.duration(this.hoursWorked).asMinutes() * 100) / (7 * 60 * numberOfDays)) -
                 (moment.duration(this.hoursAfk).asMinutes() + moment.duration(this.hoursWorked).asMinutes()) / 100);
-            this.updateCharts(dataActivitySummaryChart, dataTimeSpentChart, dataBarChart);
+            this.updateHeatMap(process);
+            this.updateCharts(dataActivitySummaryChart, dataTimeSpentChart, dataProcessChart);
         });
         this.statsService.getDnd().subscribe((obj: Dnd) => {
             this.dndActivations = obj.Activations;
